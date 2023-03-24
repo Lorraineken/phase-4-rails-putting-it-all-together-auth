e13f0a7372f9fbc631852b5e889a25e89d812cf7
Lorraineken <lorrainekupa@gmail.com>
2023-03-24 22:40:02 +0300

    Completed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+   before_action :authorize, only: [:show]
+     
+    def create 
+        user = User.create(user_params)
+         if user.valid?
+            session[:user_id] = user.id 
+            render json: user, status: :created 
+         else 
+            render json: {error:[user.errors]}, status: :unprocessable_entity
+         end      
+    end
+
+
+    def show 
+        user = User.find(session[:user_id])
+        render json: user
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :image_url, :bio)
+    end
+
+    def authorize 
+        render json: {error: user.errors}, status: :unauthorized unless session.include? :user_id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
- post '/signup', to: 'users#create' 
+ post '/signup', to: 'users#create'
+ get '/me', to: 'users#show'
+ post '/login', to: 'sessions#create'
 end
